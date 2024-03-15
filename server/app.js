a85eab14db0b8279afd28c4629fde7937ce66885
luislr220 <largoramirezluisarmando@gmail.com>
2024-03-14 23:02:52 -0600

    se agrega ruta turnos

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var empleadoRoutes = require('./routes/empleadoRoutes'); // Cambio aquí
+var turnoRoutes = require('./routes/turnoRoutes');
 
 var app = express();
 
