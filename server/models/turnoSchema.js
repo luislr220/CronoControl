d688ea01960c89dfa143e08527838fb4e4a3a3d0
CristelRR <cristel23rr@gmail.com>
2024-04-06 19:10:13 -0600

    En turno se permite utilizar contrato

--- server/models/turnoSchema.js
+++ server/models/turnoSchema.js
@@ -6,7 +6,8 @@ const turnoSchema = new mongoose.Schema({
   HoraFinal: { type: String, required: false },
   Area: { type: String, required: false }, // Corregido de Date a String
   Cupo: { type: String, required: false },
-  Estado: { type: String, required: false }
+  Estado: { type: String, required: false },
+  Contrato: {type: String, require: false}
 });
 
 module.exports = mongoose.model("Turno", turnoSchema);
