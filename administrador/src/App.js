cb1be5a0df23535b43a61801d446147cb6aab970
luislr220 <largoramirezluisarmando@gmail.com>
2024-04-09 16:33:30 -0600

    Elimincación de vistas, creación de root automatico, ocultar funciones en base a roles

--- administrador/src/App.js
+++ administrador/src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./routes/AuthContext";
+import ProtectedRoute from "./routes/ProtectedRoute";
 import AgregarUsuario from "./Components/AgregarUsuarioComponent/AgregarUsuario";
 import TurnoCrud from "./Components/TurnoCrudComponent/TurnoCrud";
 import AgregarSede from "./Components/agregarsede/agregarsede";
 import AgregarArea from "./Components/agregararea/agregararea";
 import AgregarContrato from "./Components/contratos/agregarContrato";
-import AgregarHorario from "./Components/agregarHorario/agregarHorario"; // Importa AgregarHorario aquí
 import AsignarActividades from "./Components/asiganacionActividades/actividades"; // Corregí la importación
 import ValidarVacaciones from "./Components/ValidarVacacionesComponent/ValidarVacaciones";
 import Login from "./Components/LoginFormComponent/LoginForm";
@@ -19,19 +19,18 @@ function App() {
         <Router>
           <Routes>
           <Route path="/" element={<Login />} />
-            <Route path="/agregarUsuario" element={<AgregarUsuario />} />
-            <Route path="/turnoCrud" element={<TurnoCrud />} />
-            <Route path="/agregarSede" element={<AgregarSede />} />
-            <Route path="/agregarArea" element={<AgregarArea />} />
-            <Route path="/agregarContrato" element={<AgregarContrato />} />
-            <Route path="/agregarHorario" element={<AgregarHorario />} />
-            <Route path="/validarVaca" element={<ValidarVacaciones />} />
+            <Route path="/agregarUsuario" element={<ProtectedRoute element={AgregarUsuario}/>} />
+            <Route path="/turnoCrud" element={<ProtectedRoute element={TurnoCrud} />} />
+            <Route path="/agregarSede" element={<ProtectedRoute element={AgregarSede} />} />
+            <Route path="/agregarArea" element={<ProtectedRoute element={AgregarArea} />} />
+            <Route path="/agregarContrato" element={<ProtectedRoute element={AgregarContrato} />} />
+            <Route path="/validarVaca" element={<ProtectedRoute element={ValidarVacaciones} />} />
             <Route
               key="asignar-actividades"
               path="/AsignarActividades"
-              element={<AsignarActividades />}
+              element={<ProtectedRoute element={AsignarActividades} />}
             />
-            <Route path='/validarHorario' element={<ValidarHorarios/>} />
+            <Route path='/validarHorario' element={<ProtectedRoute element={ValidarHorarios}/>} />
           </Routes>
         </Router>
       </AuthProvider>
