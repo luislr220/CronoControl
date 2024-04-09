914a9c6d578a914abaccca3092de81c19a72d6e9
luislr220 <largoramirezluisarmando@gmail.com>
2024-04-09 13:08:51 -0600

    Base de datos configurada en la nube

--- server/config/dbconfig.js
+++ server/config/dbconfig.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
-// Para guardar en la nube: const MONGODB_URL = "mongodb+srv://Cronocontrol:<cronocontrol>@cluster0.hthmjwm.mongodb.net/?retryWrites=true&w=majority";
-const MONGODB_URL = "mongodb://localhost:27017/cronocontrol";
+// Para guardar en la nube: 
+const MONGODB_URL = "mongodb+srv://Optimen:Optimen@cluster0.hthmjwm.mongodb.net/?retryWrites=true&w=majority";
+//const MONGODB_URL = "mongodb://localhost:27017/cronocontrol";
 // Configuración del registro de eventos
 mongoose.connection.on('error', console.error.bind(console, 'Error de conexión a MongoDB:'));
 mongoose.connection.on('connected', () => {
